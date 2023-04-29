58fe89ed88b4dd0e507d57b51d5f82a033fd1876
Astroweb3 <kirankumarr1901@gmail.com>
2023-04-29 12:12:37 +0530

    commit

--- exports/settings.py
+++ exports/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-te1_2!&pmq#md-xwm3+n10yx3(ggfvg#yti%4k1ig=p4mk311y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- exports/urls.py
+++ exports/urls.py
@@ -15,14 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
+
  
 urlpatterns = [
     path("admin/", admin.site.urls),
      path('',include('products.urls')),
-     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]
